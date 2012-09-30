acd9a211f14cfa26762942791946bcd5c7d88f8f
spookyjack123 <spookyjack123@gmail.com>
2012-09-29 22:14:15 -0400

    purgeV1.sh

---
+++ purgeV1.sh
@@ -0,0 +1,6 @@
+ls -1 /home/gac3/plugins/Essentials/userdata | wc -l
+# List Files beforehand
+find /home/gac3/plugins/Essentials/userdata -mtime +5 -type f|xargs rm
+# Delete files older than 5 days
+ls -1 /home/gac3/plugins/Essentials/userdata | wc -l
+# List Files afterwards
