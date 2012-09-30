3d2fbac99caaf09c55588eed46c3694a963fda82
spookyjack123 <spookyjack123@gmail.com>
2012-09-29 22:27:32 -0400

    purgeV1.sh

--- purgeV1.sh
+++ purgeV1.sh
@@ -4,3 +4,5 @@ find /home/gac3/plugins/Essentials/userdata -mtime +5 -type f|xargs rm
 # Delete files older than 5 days
 ls -1 /home/gac3/plugins/Essentials/userdata | wc -l
 # List Files afterwards
+
+# To use, change the directory to your Essentials directory 
